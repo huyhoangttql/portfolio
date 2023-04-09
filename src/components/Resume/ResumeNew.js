d3fcec383979a4fa9c0212d6f53b8fee4940a6c0
Harry Pham <harry@magestore.com>
2023-04-09 22:51:28 +0700

    update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/PhamHuyHoang_CV.pdf";
+import pdf from "../../Assets/../Assets/PhamHuyHoang.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/huyhoangttql/portfolio/master/src/Assets/PhamHuyHoang_CV.pdf";
+  "https://raw.githubusercontent.com/huyhoangttql/portfolio/master/src/Assets/PhamHuyHoang.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
